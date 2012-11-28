93ec6107f2696784305418391d1c7bfe0b9a8128
Olivier Mansour <olivier.mansour@m6.fr>
2012-11-28 14:13:06 +0100

    ajout dun timeout de 2s sur la recherche des tweets

--- app.js
+++ app.js
@@ -21,43 +21,50 @@ var twit = new twitter({
 });
 
 
+
 function handler (req, res) {
 
-  fs.readFile(__dirname + '/public/index.html',
-    function (err, data) {
-      if (err) {
-        res.writeHead(500);
-        return res.end('Error loading index.html');
-      }
-      res.writeHead(200);
-      res.end(data);
-    });
+  fs.readFile(__dirname + '/public/index.html', 'utf-8', function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+    res.writeHead(200);
+    res.write(data);
+    res.end();
 
-  // tweet history
-  twit.search('phptour OR afup OR phptournantes', {}, function(err, datas) {
-    tab = datas.results;
-    tab.reverse();
-    tab.forEach(function (data) {
-      if (data.text)
-      {
-        // check tweet !
-        if (!data.retweeted)
-        {
-          io.sockets.emit('tweet', {
-            text:       data.text,
-            user_name:  data.from_user_name,
-            user_image: data.profile_image_url_https
-          });
-        }
-      }
-    });
   });
+
+
+
+  // tweet history   res.end();
+  setTimeout( function() {
+    twit.search('phptour OR afup OR phptournantes', {}, function(err, datas) {
+      tab = datas.results;
+      tab.reverse();
+      //sleep(10000);
+      tab.forEach(function (data) {
+        if (data.text)
+        {
+          // check tweet !
+          if (!data.retweeted)
+          {
+            io.sockets.emit('tweet', {
+              text:       data.text,
+              user_name:  data.from_user_name,
+              user_image: data.profile_image_url_https
+            });
+            //console.log(data.text);
+          }
+        }
+      });
+    });
+  }, 2000);
+
 }
 
 server.listen(8080);
 console.log('Server running at http://127.0.0.1:8080/');
-
-
 
 twit.stream('statuses/filter', {track: ['phptour', 'afup', 'phptournantes'] }, function(stream) {
   stream.on('data', function (data) {
@@ -67,9 +74,6 @@ twit.stream('statuses/filter', {track: ['phptour', 'afup', 'phptournantes'] }, f
       // check tweet !
       if (!data.retweeted)
       {
-        // tweet_content.text = data.text;
-        // tweet_content.user_name = data.user.screen_name;
-        // tweet_content.user_image = data.user.profile_image_url_https;
         io.sockets.emit('tweet', {
           text:       data.text,
           user_name:  data.user.screen_name,
